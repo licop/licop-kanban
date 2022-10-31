4ed7409cad86d68623dbcf22f7cf26598d42a6cd
licop <licop@users.noreply.github.com>
2022-10-31 17:57:53 +0800

    add css-in-js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
+/** @jsxImportSource @emotion/react */
 import React, { useState } from 'react';
+import { css } from '@emotion/react';
 import logo from './logo.svg';
 import './App.css';
 
-
-// const todoList = [
-//   { title: '开发任务-1', status: '22-05-22 18:15' },
-//   { title: '开发任务-3', status: '22-05-22 18:15' },
-//   { title: '开发任务-5', status: '22-05-22 18:15' },
-//   { title: '测试任务-3', status: '22-05-22 18:15' }
-// ];
 const ongoingList = [
   { title: '开发任务-4', status: '22-05-22 18:15' },
   { title: '开发任务-6', status: '22-05-22 18:15' },
@@ -19,11 +14,74 @@ const doneList = [
   { title: '测试任务-1', status: '22-05-22 18:15' }
 ];
 
+const kanbanBoardStyles = css`
+  flex: 10;
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  margin: 0 1rem 1rem;
+`;
+
+const kanbanColumnStyles = css`
+  flex: 1 1;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid gray;
+  border-radius: 1rem;
+
+  & > h2 {
+    margin: 0.6rem 1rem;
+    padding-bottom: 0.6rem;
+    border-bottom: 1px solid gray;
+
+    & > button {
+      float: right;
+      margin-top: 0.2rem;
+      padding: 0.2rem 0.5rem;
+      border: 0;
+      border-radius: 1rem;
+      height: 1.8rem;
+      line-height: 1rem;
+      font-size: 1rem;
+    }
+  }
+
+  & > ul {
+    flex: 1;
+    flex-basis: 0;
+    margin: 1rem;
+    padding: 0;
+    overflow: auto;
+  }
+`;
+
+export const kanbanCardStyles = css`
+  margin-bottom: 1rem;
+  padding: 0.6rem 1rem;
+  border: 1px solid gray;
+  border-radius: 1rem;
+  list-style: none;
+  background-color: rgba(255, 255, 255, 0.4);
+  text-align: left;
+
+  &:hover {
+    box-shadow: 0 0.3rem 0.3rem rgba(0, 0, 0, 0.3), inset 0 1px #fff;
+  }
+`;
+
+export const kanbanCardTitleStyles = css`
+  min-height: 3rem;
+`;
+
 const KanbanCard = ({ title, status }) => {
   return (
-    <li className='kanban-card'>
-      <div className='card-title'>{title}</div>
-      <div className='card-status'>{status}</div>
+    <li css={kanbanCardStyles}>
+      <div css={kanbanCardTitleStyles}>{title}</div>
+      <div css={css`
+        text-align: right;
+        font-size: 0.8rem;
+        color: #333;
+      `}>{status}</div>
     </li>
   )
 }
@@ -40,15 +98,25 @@ const KanbanNewCard = ({ onSubmit }) => {
   }
 
   return (
-    <li className='kanban-card'>
+    <li css={kanbanCardStyles}>
       <h3>添加新卡片</h3>
-      <div className='card-title'>
+      <div css={kanbanCardTitleStyles}>
         <input type='text' value={title} onChange={handleChange} onKeyDown={handleKeyDown}  />
       </div>
     </li>
   )
 }
 
+const KanbanBoard = ({ children }) => (
+  <main css={kanbanBoardStyles} >{children}</main>
+)
+  
+const KanbanColumn = ({children, className, title}) => (
+  <section className={className} css={kanbanColumnStyles}>
+    <h2>{title}</h2>
+    <ul>{children}</ul>
+  </section>
+)
 
 function App() {
   const [showAdd, setShowAdd] = useState(false);
@@ -76,33 +144,30 @@ function App() {
         <h1>我的看板</h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <main className='kanban-board'>
-        <section className='kanban-column column-todo'>
-          <h2>待处理<button onClick={handleAdd} disabled={showAdd}>&#8853; 添加新卡片</button></h2>
-          <ul>
-            {showAdd && <KanbanNewCard onSubmit={handleSubmit} />}
-            {
-              todoList.map(props => <KanbanCard key={props.title} {...props} />)
-            }
-          </ul>
-        </section>
-        <section className='kanban-column column-ongoing'>
-          <h2>进行中</h2>
-          <ul>
-            {
-              ongoingList.map(props => <KanbanCard key={props.title} {...props} />)
-            }
-          </ul>
-        </section>
-        <section className='kanban-column column-done'>
-          <h2>已完成</h2>
+      <KanbanBoard>
+        <KanbanColumn className='column-todo' title={
+          <>    
+            待处理<button onClick={handleAdd} disabled={showAdd}>&#8853; 添加新卡片</button>  
+          </>
+        }>
+          {showAdd && <KanbanNewCard onSubmit={handleSubmit} />}
+          {
+            todoList.map(props => <KanbanCard key={props.title} {...props} />)
+          }
+        </KanbanColumn>
+        <KanbanColumn className='column-ongoing' title="进行中" >
+          {
+            ongoingList.map(props => <KanbanCard key={props.title} {...props} />)
+          }
+        </KanbanColumn>
+        <KanbanColumn className='column-done' title="已完成">
           <ul>
             {
               doneList.map(props => <KanbanCard key={props.title} {...props} />)  
             }
           </ul>
-        </section>
-      </main>
+        </KanbanColumn>
+      </KanbanBoard>
     </div>
   );
 }
